ccf91739de6f20534c2b305c879f62bc5507fbc9
dipamhrni05 <dipamhrni05@gmail.com>
2026-05-05 12:10:36 +0800

    task-2

---
+++ my-app/app/learn/page.tsx
@@ -0,0 +1,43 @@
+export default function Learn() {
+    return (
+    
+    
+    <div className="bg-white h-screen flex gap-4 justify-center items-center">
+
+        <div className="bg-purple-300 h-72 w-90 flex flex-col justify-between p-8 rounded-4xl" >
+        <div className=" flex gap-4">
+            <img src="/palette.svg" />
+            <p>Design</p>
+        </div>
+        <div>
+            <h1 className= "text-4xl font-bold">Figma</h1>
+            <p>8 hours ago</p>
+        </div>
+        </div> 
+
+        <div className="bg-orange-300 h-72 w-90 flex flex-col justify-between p-8 rounded-4xl" >
+        <div className=" flex gap-4">
+            <img src="/palette.svg" />
+            <p>Design</p>
+        </div>
+        <div>
+            <h1 className= "text-4xl font-bold">Phyton</h1>
+            <p>2 days ago</p>
+        </div>
+        </div>
+
+        <div className="bg-red-300 h-72 w-90 flex flex-col justify-between p-8 rounded-4xl" >
+        <div className=" flex gap-4">
+            <img src="/palette.svg" />
+            <p>Design</p>
+        </div>
+        <div>
+            <h1 className= "text-4xl font-bold">Sketch</h1>
+            <p>4 days ago</p>
+        </div>
+        </div>
+
+
+
+    </div>
+)};
---
+++ my-app/app/task-2/page.tsx
@@ -0,0 +1,67 @@
+export default function Task() {
+    return (
+    <div className="bg-white h-screen flex gap-4 justify-center items-center">
+
+        <div className="bg-cyan-50 h-72 w-96 flex flex-col justify-between p-8 rounded-4xl text-blue-900">
+            
+            <div className=" flex gap-2">
+                <img src= "/palette.svg" className="w-4 h-4"/>
+                <p className="text-sm">Design</p>
+            </div>
+
+
+            <div >
+                <h1 className="text-2xl font-bold">Adobe Photoshop</h1>
+                <div className="flex justify-between  pt-2">
+                    <p className="text-sm">in 3 days</p>
+                    <div className="flex -space-x-3 items-center">
+                        <img src="/keisya.jpg" className="w-6 h-6 rounded-full border-2 border-white" />
+                        <img src="/lyodra.jpg" className="w-6 h-6 rounded-full border-2 border-white" />
+                        <img src="/mahalini.jpg" className="w-6 h-6 rounded-full border-2 border-white" />
+
+                        <div className="flex items-center w-6 h-6 rounded-full bg-blue-100 p-1.5 text-xs scale-90">
+                        +3
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div className="bg-green-50 h-72 w-96 flex flex-col justify-between p-8 rounded-4xl text-green-900">
+        
+            <div className="flex justify-between items-center" >
+                <div className="flex gap-2">
+                    <img src= "/terminal-2.svg" className="w-4 h-4"/>
+                    <p className="text-sm">AI</p>
+                 </div>
+                <img src= "/star.svg" className="w-4 h-4"/>
+            </div>
+        
+
+            <div>
+                <h1 className="text-2xl font-bold">DALL·E 2, Midjourney, Stable Diffusion</h1>
+
+                <div className="flex justify-between pt-2">
+                    <p className="text-sm">in 5 days</p>
+                    <div className="flex -space-x-3 items-center">
+                        <img src="/tiara.jpg" className="w-6 h-6 rounded-full border-2 border-white" />
+                        <img src="/vanesa.jpg" className="w-6 h-6 rounded-full border-2 border-white"/>
+                        <img src="/ziva.jpg" className="w-6 h-6 rounded-full border-2 border-white"/>
+
+                        <div className="flex items-center w-6 h-6 rounded-full bg-green-100 p-1.5 text-xs scale-90">
+                        +3
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+
+
+
+
+    </div>
+)};
